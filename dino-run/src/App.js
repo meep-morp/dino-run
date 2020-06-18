fbd37ab8141f683745c54f6e088add42aa785b98
meep-morp <gracieaerogirl@gmail.com>
2020-06-18 14:17:56 -0500

    Added title screen

--- dino-run/src/App.js
+++ dino-run/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Trex from './game/components/Trex';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "./styles/gameEngine.css"
 import { useState } from 'react';
 import Obstacle from './game/components/Obstacle';
@@ -11,16 +12,22 @@ function App() {
   const player = useRef(null)
   const entity = useRef(null)
 
-  const [count, setCount] = useState(1);
-  const [tick, setTick] = useState(0);
-  const [isAlive, setIsAlive] = useState(true);
+  const [isAlive, setIsAlive] = useState(false);
   const [score, setScore] = useState(0);
 
   useEffect(() => {
+    console.log("count")
     window.setInterval(() => {
-          setScore(score => score + 10)
+      isAlive ? setScore(score => score + 10) : setScore(0);
     }, 300)
-  }, [isAlive]);
+  }, []);
+
+  useEffect(() => {
+    if (window.location.pathname == "/start") {
+      console.log("Start")
+      setIsAlive(true);
+    }
+  }, [])
 
   // useEffect(() => {
   //   console.log(player)
@@ -29,18 +36,28 @@ function App() {
 
   // useEffect(() => {
   //   isColliding(player, entity) ? setIsAlive(false) : setIsAlive(true);
-  // }, [])
+  // }, [score])
 
   // setInterval(() => {
   //   setTick(tick + 1)
   // }, 1000)
 
   return (
-    <div className="game">
-      <div className="score">{score}</div>
-      <Trex pos={tick} ref={player} />
-      <Obstacle pos={tick} ref={entity} />
-    </div>
+    <Router>
+      <Route path="/" exact>
+        <div className="home">
+          <h1>DINO RUN</h1>
+          <Link className="button" to="/start">Start Game</Link>
+        </div>
+      </Route>
+      <Route path="/start">
+        <div className="game">
+          <div className="score">{score}</div>
+          <Trex ref={player} />
+          <Obstacle ref={entity} />
+        </div>
+      </Route>
+    </Router>
   );
 }
 
